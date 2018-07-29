37c76041b43120e89260989b6e9196d7695e7f78
kidnapsteal <aldirenaldigunawan@gmail.com>
2018-07-29 23:17:00 +0700

    Init Dagger App Module and app component

---
+++ app/src/main/java/com/kidnapsteal/coincommunity/di/AppComponent.kt
@@ -0,0 +1,21 @@
+package com.kidnapsteal.coincommunity.di
+
+import android.app.Application
+import com.kidnapsteal.coincommunity.CoinCommunity
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AppModule::class, AndroidSupportInjectionModule::class])
+interface AppComponent : AndroidInjector<CoinCommunity> {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(app: Application): Builder
+
+        fun build(): AppComponent
+    }
+}
---
+++ app/src/main/java/com/kidnapsteal/coincommunity/di/AppModule.kt
@@ -0,0 +1,13 @@
+package com.kidnapsteal.coincommunity.di
+
+import android.app.Application
+import android.content.Context
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class AppModule {
+
+    @Binds
+    abstract fun bindContext(app: Application): Context
+}
